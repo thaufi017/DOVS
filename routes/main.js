b74656da17d57b97385deac7d6782d313457fc9a
thaufi017 <sthaufiths@gmail.com>
2024-04-26 02:25:37 +0530

    commited

--- routes/main.js
+++ routes/main.js
@@ -1,23 +1,7 @@
 var express = require('express');
 var router = express.Router();
-/* GET users listing. */
-// const express=require('express');
-// const app=express()
 var conn = require('../database');
-
-router.get('/form', function (req, res, next) {
-  // res.render('voter-registration.ejs');
-  if (req.session.loggedinUser) {
-    res.render('voter-registration.ejs')
-  } else {
-    res.redirect('/login');
-  }
-});
-
-
 var getAge = require('get-age');
-
-
 var nodemailer = require('nodemailer');
 var rand = Math.floor((Math.random() * 10000) + 54);
 var transporter = nodemailer.createTransport({
@@ -30,29 +14,31 @@ var transporter = nodemailer.createTransport({
 
 var account_address;
 var data;
+var firstName; // Variable to store the first name
 
-// app.use(express.static('public'));
-// //app.use('/css',express.static(__dirname+'public/css'));
-// //app.use(express.json());
-// app.use(express.urlencoded());
+router.get('/form', function (req, res, next) {
+  if (req.session.loggedinUser) {
+    res.render('voter-registration.ejs')
+  } else {
+    res.redirect('/login');
+  }
+});
 
 router.post('/registerdata', function (req, res) {
   var dob = [];
-  data = req.body.aadharno;    //data stores aadhar no
-  console.log(data);
-  account_address = req.body.account_address;     //stores metamask acc address
-  //console.log(data);
+  data = req.body.aadharno;
+  account_address = req.body.account_address;
   let sql = "SELECT * FROM aadhar_info WHERE Aadharno = ?";
   conn.query(sql, data, (error, results, fields) => {
     if (error) {
       return console.error(error.message);
     }
-    //console.log(results)
     dob = results[0].Dob;
     var email = results[0].Email;
-    age = getAge(dob);
-    is_registerd = results[0].Is_registered;
-    if (is_registerd != 'YES') {
+    firstName = results[0].Name; // Store the first name
+    var age = getAge(dob);
+    var is_registered = results[0].Is_registered;
+    if (is_registered != 'YES') {
       if (age >= 18) {
         var mailOptions = {
           from: 'dovs.blockchain@gmail.com',
@@ -74,32 +60,24 @@ router.post('/registerdata', function (req, res) {
         res.send('You cannot vote as your age is less than 18');
       }
     }
-    else    //IF USER ALREADY REGISTERED
-    {
+    else {
       res.render('voter-registration.ejs', { alertMsg: "You are already registered. You cannot register again" });
     }
-
   });
-
-  //console.log(dob);
-  //console.log(age);
-  //res.send("ok")
-  //console.log(dob);
-})
+});
 
 router.post('/otpverify', (req, res) => {
   var otp = req.body.otp;
   if (otp == rand) {
-    var record = { Account_address: account_address, Is_registered: 'Yes' };
+    var record = { Account_address: account_address, Is_registered: 'Yes', Name: firstName }; // Include First_name here
     var sql = "INSERT INTO registered_users SET ?";
     conn.query(sql, record, function (err2, res2) {
       if (err2) {
         throw err2;
       }
       else {
-        var sql1 = "Update aadhar_info set Is_registered=? Where Aadharno=?";
-        var record1 = ['YES', data]
-        console.log(data)
+        var sql1 = "UPDATE aadhar_info SET Is_registered = ? WHERE Aadharno = ?";
+        var record1 = ['YES', data];
         conn.query(sql1, record1, function (err1, res1) {
           if (err1) {
             res.render('voter-registration.ejs');
@@ -107,32 +85,15 @@ router.post('/otpverify', (req, res) => {
           else {
             console.log("1 record updated");
             var msg = "You are successfully registered";
-            // res.send('You are successfully registered');
             res.render('voter-registration.ejs', { alertMsg: msg });
           }
         });
-
       }
     });
   }
   else {
     res.render('voter-registration.ejs', { alertMsg: "Session Expired! , You have entered wrong OTP " });
   }
-})
-
-
-
-// router.get('/register',function(req,res){
-//     res.sendFile(__dirname+'/views/index.html')
-// });
-
-/*app.get('/signin_signup',function(req,res){
-    res.sendFile(__dirname+'/views/signup.html')
 });
-
-app.get('/signup',function(req,res){
-    console.log(req.body);
-    res.sendFile(__dirname+'/views/signup.html')
-});*/
 
 module.exports = router;
--- src/js/app.js
+++ src/js/app.js
@@ -2,64 +2,74 @@ var phaseEnum; // for changing phases of voting
 App = {
   web3Provider: null,
   contracts: {},
-  account:0x0,
+  account: '0x0', // Initialize account as a string
 
-  init: async function() {
+  init: async function () {
     // Load pets.
-   
+
     return await App.initWeb3();
   },
 
-  initWeb3: async function() {
+  initWeb3: async function () {
     if (window.ethereum) {
       try {
+        // Request account access
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setAccounts(accounts);
+        // Set the account
+        App.account = accounts[0];
       } catch (error) {
         if (error.code === 4001) {
           // User rejected request
+          console.error('User rejected MetaMask request');
+        } else {
+          console.error('Error occurred while requesting MetaMask accounts:', error);
         }
       }
+    } else {
+      console.error('MetaMask extension not detected');
     }
 
     return App.initContract();
   },
 
-  initContract: function() {
-    
-    $.getJSON("Contest.json",function(contest){
-      App.contracts.Contest=TruffleContract(contest);
-
-      App.contracts.Contest.setProvider(web3.currentProvider);
-
-  
+  initContract: function () {
+    $.getJSON("Contest.json", function (contest) {
+      App.contracts.Contest = TruffleContract(contest);
+      // Check if MetaMask is available
+      if (window.ethereum) {
+        // Use MetaMask provider
+        App.contracts.Contest.setProvider(window.ethereum);
+      } else {
+        console.error('MetaMask extension not detected');
+      }
       return App.render();
     });
   },
 
-  render: function(){
-    
+
+  render: function () {
+
     var contestInstance;
-    var loader=$("#loader");
-    var content=$("#content"); 
+    var loader = $("#loader");
+    var content = $("#content");
     loader.show();
     content.hide();
     $("#after").hide();
 
-    web3.eth.getCoinbase(function(err,account){
-      if(err===null){
-        App.account=account;
-        $("#accountAddress").html("Your account: "+account);
-      }
-    });
+    // web3.eth.getCoinbase(function (err, account) {
+    // if (err === null) {
+    // App.account = account;
+    //$("#accountAddress").html("Your account: " + account);
+    //}
+    // });
 
-    
+
 
     // ------------- fetching candidates to front end from blockchain code-------------
-    App.contracts.Contest.deployed().then(function(instance){
-      contestInstance=instance;
+    App.contracts.Contest.deployed().then(function (instance) {
+      contestInstance = instance;
       return contestInstance.contestantsCount();
-    }).then(function(contestantsCount){
+    }).then(function (contestantsCount) {
       // var contestantsResults=$("#contestantsResults");
       // contestantsResults.empty();
 
@@ -81,187 +91,216 @@ App = {
       //     var contestantOption="<option value='"+id+"'>"+name+"</option>";
       //     contestantSelect.append(contestantOption);
 
-      var contestantsResults=$("#test");
+      var contestantsResults = $("#test");
       contestantsResults.empty();
-      var contestantsResultsAdmin=$("#contestantsResultsAdmin");
+      var contestantsResultsAdmin = $("#contestantsResultsAdmin");
       contestantsResultsAdmin.empty();
 
-      var contestantSelect=$("#contestantSelect");
+      var contestantSelect = $("#contestantSelect");
       contestantSelect.empty();
 
-      for(var i=1; i<=contestantsCount; i++){
-        contestInstance.contestants(i).then(function(contestant){
-          var id=contestant[0];
-          var name=contestant[1];
-          var voteCount=contestant[2];
-          var fetchedParty=contestant[3];
+      for (var i = 1; i <= contestantsCount; i++) {
+        contestInstance.contestants(i).then(function (contestant) {
+          var id = contestant[0];
+          var name = contestant[1];
+          var voteCount = contestant[2];
+          var fetchedParty = contestant[3];
           var fetchedAge = contestant[4];
           var fetchedQualification = contestant[5]
 
-          var contestantTemplate="<div class='card' style='width: 15rem; margin: 1rem;'><img class='card-img-top'src='../img/Sample_User_Icon.png' alt=''><div class='card-body text-center'><h4 class='card-title'>" 
-          + name + "</h4>"+
-          "<button type='button' class='btn btn-info' data-toggle='modal' data-target='#modal"+id+"'>Click Here to Vote</button>" 
-          + "<div class='modal fade' id='modal"+id+"' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>"
-          + "<div class='modal-dialog modal-dialog-centered' role='document'>"
-          + "<div class='modal-content'>"
-          + "<div class='modal-header'>"
-          + "<h5 class='modal-title' id='exampleModalLongTitle'> <b>" + name + "</b></h5>"
-          + "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"
-          + "</div>"
-          + "<div class='modal-body'> <b> Party : " + fetchedParty +"<br>Age : " + fetchedAge + "<br>Qualification : " + fetchedQualification + "<br></b></div>"
-          + "<div class='modal-footer'>"
-          + "<button class='btn btn-info' onClick='App.castVote("+id.toString()+")'>VOTE</button>"
-          +"<button type='button' class='btn btn-info' data-dismiss='modal'>Close</button></div>"
-          + "</div></div></div>"
-          + "</div></div>";
-          contestantsResults.append(contestantTemplate)  ;
+          var contestantTemplate = "<div class='card' style='width: 15rem; margin: 1rem;'><img class='card-img-top'src='../img/Sample_User_Icon.png' alt=''><div class='card-body text-center'><h4 class='card-title'>"
+            + name + "</h4>" +
+            "<button type='button' class='btn btn-info' data-toggle='modal' data-target='#modal" + id + "'>Click Here to Vote</button>"
+            + "<div class='modal fade' id='modal" + id + "' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>"
+            + "<div class='modal-dialog modal-dialog-centered' role='document'>"
+            + "<div class='modal-content'>"
+            + "<div class='modal-header'>"
+            + "<h5 class='modal-title' id='exampleModalLongTitle'> <b>" + name + "</b></h5>"
+            + "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"
+            + "</div>"
+            + "<div class='modal-body'> <b> Party : " + fetchedParty + "<br>Age : " + fetchedAge + "<br>Qualification : " + fetchedQualification + "<br></b></div>"
+            + "<div class='modal-footer'>"
+            + "<button class='btn btn-info' onClick='App.castVote(" + id.toString() + ")'>VOTE</button>"
+            + "<button type='button' class='btn btn-info' data-dismiss='modal'>Close</button></div>"
+            + "</div></div></div>"
+            + "</div></div>";
+          contestantsResults.append(contestantTemplate);
 
-          var contestantOption="<option style='padding: auto;' value='"+id+"'>"+name+"</option>";
+          var contestantOption = "<option style='padding: auto;' value='" + id + "'>" + name + "</option>";
           contestantSelect.append(contestantOption);
 
-          var contestantTemplateAdmin="<tr><th>"+id+"</th><td>"+name+"</td><td>"+fetchedAge+"</td><td>"+fetchedParty+"</td><td>"+fetchedQualification+"</td><td>"+voteCount+"</td></tr>";
-          contestantsResultsAdmin.append(contestantTemplateAdmin)  ;
-        }); 
+          var contestantTemplateAdmin = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + fetchedAge + "</td><td>" + fetchedParty + "</td><td>" + fetchedQualification + "</td><td>" + voteCount + "</td></tr>";
+          contestantsResultsAdmin.append(contestantTemplateAdmin);
+        });
       }
       loader.hide();
       content.show();
-    }).catch(function(error){
+    }).catch(function (error) {
       console.warn(error);
     });
 
     // ------------- fetching current phase code -------------
-    App.contracts.Contest.deployed().then(function (instance){
+    App.contracts.Contest.deployed().then(function (instance) {
       return instance.state();
-    }).then(function(state){
+    }).then(function (state) {
       var fetchedState;
       var fetchedStateAdmin;
       phaseEnum = state.toString();
-      if(state == 0){
+      if (state == 0) {
         fetchedState = "Registration phase is on , go register yourself to vote !!";
         fetchedStateAdmin = "Registration";
-      }else if(state == 1){
+      } else if (state == 1) {
         fetchedState = "Voting is now live !!!";
         fetchedStateAdmin = "Voting";
-      }else {
+      } else {
         fetchedState = "Voting is now over !!!";
         fetchedStateAdmin = "Election over";
       }
-      
+
       var currentPhase = $("#currentPhase");//for user
       currentPhase.empty();
       var currentPhaseAdmin = $("#currentPhaseAdmin");//for admin
       currentPhaseAdmin.empty();
-      var phaseTemplate = "<h1>"+fetchedState+"</h1>";
-      var phaseTemplateAdmin = "<h3> Current Phase : "+fetchedStateAdmin+"</h3>";
+      var phaseTemplate = "<h1>" + fetchedState + "</h1>";
+      var phaseTemplateAdmin = "<h3> Current Phase : " + fetchedStateAdmin + "</h3>";
       currentPhase.append(phaseTemplate);
       currentPhaseAdmin.append(phaseTemplateAdmin);
-    }).catch(function(err){
+    }).catch(function (err) {
       console.error(err);
     })
 
     // ------------- showing result -------------
-    App.contracts.Contest.deployed().then(function (instance){
+    App.contracts.Contest.deployed().then(function (instance) {
       return instance.state();
-    }).then(function(state){
+    }).then(function (state) {
       var result = $('#Results');
-      if(state == 2){
+      if (state == 2) {
         $("#not").hide();
-        contestInstance.contestantsCount().then(function(contestantsCount){
-          for(var i=1; i<=contestantsCount; i++){
-            contestInstance.contestants(i).then(function(contestant){
-              var id=contestant[0];
-              var name=contestant[1];
-              var voteCount=contestant[2];
-              var fetchedParty=contestant[3];
+        contestInstance.contestantsCount().then(function (contestantsCount) {
+          for (var i = 1; i <= contestantsCount; i++) {
+            contestInstance.contestants(i).then(function (contestant) {
+              var id = contestant[0];
+              var name = contestant[1];
+              var voteCount = contestant[2];
+              var fetchedParty = contestant[3];
               var fetchedAge = contestant[4];
               var fetchedQualification = contestant[5];
 
-              var resultTemplate="<tr><th>"+id+"</th><td>"+name+"</td><td>"+fetchedAge+"</td><td>"+fetchedParty+"</td><td>"+fetchedQualification+"</td><td>"+voteCount+"</td></tr>";
-              result.append(resultTemplate)  ;
+              var resultTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + fetchedAge + "</td><td>" + fetchedParty + "</td><td>" + fetchedQualification + "</td><td>" + voteCount + "</td></tr>";
+              result.append(resultTemplate);
             });
           }
         })
-         
+
       } else {
         $("#renderTable").hide();
       }
-    }).catch(function(err){
+    }).catch(function (err) {
       console.error(err);
     })
   },
 
-  
-  
+
+
 
   // ------------- voting code -------------
-  castVote: function(id){
-    
-    var contestantId=id;
-    App.contracts.Contest.deployed().then(function (instance){
-      return instance.vote(contestantId,{from: App.account});
-    }).then(function(result){
-      // $("#test").hide();
-      // $("#after").show();
-    }).catch(function(err){
-      console.error(err);
-    })
+  castVote: function (id) {
+    var contestantId = id;
 
+    if (window.ethereum) {
+      window.ethereum.request({ method: 'eth_requestAccounts' }).then(function (accounts) {
+        var selectedAccount = accounts[0];
+
+        // Now that we have the selected account, we can call the vote function without specifying the 'from' address
+        App.contracts.Contest.deployed().then(function (instance) {
+          return instance.vote(contestantId, { from: selectedAccount });
+        }).then(function (result) {
+          // Voting successful
+          console.log('Vote cast successfully');
+        }).catch(function (err) {
+          console.error('Error casting vote:', err);
+        });
+      }).catch(function (error) {
+        if (error.code === 4001) {
+          // User rejected request
+          console.error('User rejected MetaMask request');
+        } else {
+          console.error('Error occurred while requesting MetaMask accounts:', error);
+        }
+      });
+    } else {
+      console.error('MetaMask extension not detected');
+    }
   },
 
+
+
+
   // ------------- adding candidate code -------------
-  addCandidate: function(){
+  addCandidate: function () {
     $("#loader").hide();
-    var name=$('#name').val();
+    var name = $('#name').val();
     var age = $('#age').val();
     var party = $('#party').val();
     var qualification = $('#qualification').val();
-    
-    App.contracts.Contest.deployed().then(function(instance){
-      return instance.addContestant(name,party,age,qualification);
-    }).then(function(result){
+
+    App.contracts.Contest.deployed().then(function (instance) {
+      return instance.addContestant(name, party, age, qualification, { from: App.account });
+    }).then(function (result) {
       $("#loader").show();
       $('#name').val('');
       $('#age').val('');
       $('#party').val('');
       $('#qualification').val('');
-    }).catch(function(err){
+    }).catch(function (err) {
       console.error(err);
     })
   },
+
 
   // ------------- changing phase code -------------
-  
-  changeState: function(){
-    phaseEnum ++;
+
+  changeState: function () {
+    phaseEnum++;
     // console.log(phaseEnum);
-    App.contracts.Contest.deployed().then(function(instance){
-      return instance.changeState(phaseEnum);
-    }).then(function(result){
+    App.contracts.Contest.deployed().then(function (instance) {
+      // Specify the transaction options including the 'from' address
+      var txOptions = {
+        from: App.account
+      };
+      return instance.changeState(phaseEnum, txOptions); // Pass the transaction options to the contract function
+    }).then(function (result) {
       $("#content").hide();
       $("#loader").show();
-    }).catch(function(err){
+    }).catch(function (err) {
       console.error(err);
     })
   },
 
+
   // ------------- registering voter code -------------
-  registerVoter: function(){
-    var add=$('#accadd').val();
-    App.contracts.Contest.deployed().then(function(instance){
-      return instance.voterRegisteration(add);
-    }).then(function(result){
+  registerVoter: function () {
+    var add = $('#accadd').val();
+    App.contracts.Contest.deployed().then(function (instance) {
+      // Specify the transaction options including the 'from' address
+      var txOptions = {
+        from: App.account
+      };
+      return instance.voterRegisteration(add, txOptions); // Pass the transaction options to the contract function
+    }).then(function (result) {
       $("#content").hide();
       $("#loader").show();
-    }).catch(function(err){
+    }).catch(function (err) {
       console.error(err);
     })
   }
 
+
 };
 
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
